865350bb5e56bdfef603a83fa84a535aaf206a5c
iskander alymov <iska.alymov@gmail.com>
2021-05-29 22:51:03 +0600

    changed index.php

---
+++ index.php
@@ -0,0 +1,326 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Iskander Alymov - Portfolio</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="img/icon.png">
+		<link href="https://fonts.googleapis.com/css?family=Fira+Sans:300i,400,400i,500,600,700,800,900" rel="stylesheet">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/slicknav.min.css">
+		<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <link rel="stylesheet" href="css/owl.theme.default.min.css">
+        <link rel="stylesheet" href="css/animate.min.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">
+		
+		<!-- CSS -->
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+    </head>
+    <body>
+
+		<!-- Header Area -->
+		<header id="header" class="header">
+			<div class="header-inner">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-sm-2 col-xs-12">
+							<!-- <div class="logo">
+								<a href="index.html"><img src="img/nnn.png" alt="logo.png"></a>
+							</div>-->
+						</div>
+						<div class="col-md-9 col-sm-10 col-xs-12">
+							<div class="mobile-menu"></div>
+							<nav class="navbar navbar-default">
+								<div class="collapse navbar-collapse">
+									<ul id="nav" class="nav navbar-nav">
+										<li class="current"><a href="#slider">Welcome</a></li>
+										<li><a href="#about">About Me</a></li>
+										<li><a href="#skill">Skill</a></li>
+										<li><a href="#story">Story</a></li>
+										<li><a href="#contact">Contact</a></li>
+									</ul>
+								</div> 
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Header Inner -->
+		</header>
+		<!-- End Header Area -->
+		
+		<!-- Slider Area -->
+		<section id="slider" class="slider" style="background-image:url('img/KK.jpg')">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-7 col-sm-12 col-xs-12">
+						<div class="text">
+							<h1>Hi,my name is Iskander</h1>
+							<p>My full name is Iskander Alymov, I'm 17 and I'm learning in International Ala-Too University's college, on Software Development specialty. </p>
+							<div class="button">
+								<a href="#" class="btn primary "><i class="fa fa-briefcase"></i>view work</a>
+								<a href="#contact" class="btn"><i class="fa fa-phone"></i>Contact me</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ End Slider Area -->
+		
+		<!-- Start about -->
+		<section id="about" class="about">
+			<div class="container">
+				<div class="row">
+					<div class="about-content">
+						<div class="col-md-2 col-sm-2 col-xs-12">
+							<div class="section-title">
+								<h2>About <span>Me</span></h2>
+							</div>
+						</div>
+						<div class="col-md-6  col-sm-6 col-xs-12">
+							<div class="single-about">
+								<p class="bolt">Something else about myself.</p>
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+								Doloremque beatae ad dolor eius impedit nulla incidunt praesentium tempora facere reiciendis.
+								 Assumenda quidem vero vel illo. Beatae qui officiis recusandae dolorum.	
+								</p>
+								<p class="bolt">Lorem ipsum dolor sit</p>
+								<div class="social">
+									<ul>
+										
+										<li><a href="https://www.instagram.com/htk_is/"><i class="fa fa-instagram"></i></a></li>
+										<li><a href="https://www.pinterest.ru/iskaalymov/_saved/"><i class="fa fa-pinterest-p"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4 col-xs-12">
+							<div class="image">
+								<img src="img/kk2.jpg" alt="#">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ End about -->	
+		
+		<!-- Start skill -->
+		<section id="skill" class="skill section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-2 col-sm-12 col-xs-12">
+						<div class="section-title">
+							<h2>My <span>Skill</span></h2>
+						</div>
+					</div>
+					<div class="col-md-10 col-sm-12 col-xs-12">
+						<div class="skill-head">
+							<div class="row">
+								<div class="col-md-6 col-sm-12 col-xs-12">
+									<div class="skill-content">
+										<h3>ABOUT MY SKILLS</h3>
+										
+										<p> lorem ipsum</p>
+										<p><b> 
+										Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+										 Mollitia autem ipsum adipisci voluptates corporis suscipit vitae, atque dolores doloribus ut odio alias tempore sed officiis ex facilis dolore assumenda id! </b></p>
+									</div>
+								</div>
+								<div class="col-md-6 col-sm-12 col-xs-12">
+									<div class="skill-main">
+										<div class="single-skill">
+											<div class="skill-title">
+												<h4>C++</h4>
+											</div>
+											<div class="progress two">
+												<div class="progress-bar" data-percent="45">
+													<span>45%</span>
+												</div>
+											</div>
+										</div>
+										<div class="single-skill">
+											<div class="skill-title">
+												<h4>HTML & CSS3</h4>
+											</div>
+											<div class="progress two">
+												<div class="progress-bar" data-percent="60">
+													<span>60%</span>
+												</div>
+											</div>
+										</div>
+										<div class="single-skill">
+											<div class="skill-title">
+												<h4>PYTHON</h4>
+											</div>
+											<div class="progress two">
+												<div class="progress-bar" data-percent="30">
+													<span>30%</span>
+												</div>
+											</div>
+										</div>
+										
+										
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ End skill -->	
+		
+		<!-- Start Story -->
+		<section id="story" class="story section">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="section-title">
+							<h2>my <span>Story</span></h2>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
+						<div class="story-content">
+							<!-- single-story -->
+							<div class="single-story">
+								<span class="year wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.4s">2017</span>
+								<div class="inner-content wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">
+									<h3>Started to learn HTML5 & CSS3</h3>
+									
+									<p class="p2">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+									Officia sed temporibus maiores! Mollitia a quaerat accusamus quod incidunt modi,
+									</p>
+								</div>
+							</div>
+							<!-- single-story -->
+							<div class="single-story">
+								<span class="year wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.6s">2019</span>
+								<div class="inner-content wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s">
+									<h3>Entered to AIU's college</h3>
+									
+									<p class="p2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+									Reprehenderit dolor obcaecati in voluptate a nemo aut corrupti officiis? Nihil sit unde quis sed a labore incidunt nisi ipsam aliquam ea.
+									</p>
+								</div>
+							</div>
+							<!-- single-story -->
+							<div class="single-story">
+								<span class="year wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.8s">2020</span>
+								<div class="inner-content wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.8s">
+									<h3>Learning C++ and continue to learn WEB</h3>
+									
+									<p class="p2">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+									Quas amet aliquid vero dicta ad quis dolorum corporis, magnam dolore officia voluptatem laborum eum quaerat nulla nisi dolor placeat? Facilis, accusamus.
+									</p>
+								</div>
+							</div>
+							
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ End story -->	
+		
+		<!-- Contact -->
+		<section id="contact" class="contact section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 col-sm-12 col-xs-12">
+						<div class="contact-info">
+							<h4>Contact info</h4>
+						</br>
+							<span><i class="fa fa-phone-square"></i>+996 ### ## ## ##</span>
+							<span><i class="fa fa-map-marker"></i>Kyrgyzstan, Bishkek, 5th microdistrict</span>
+							<span><i class="fa fa-envelope"></i><a href="#">######@gmail.com</a></span>
+							<span><i class="fa fa-globe"></i><a href="#">www.#########.com</a></span>
+						</div>
+					</div>
+					<div class="col-md-8 col-sm-12 col-xs-12 ">
+						<div class="form-head">
+							<form class="form" action="#">
+								<div class="form-group">
+									<input name="name" type="text" placeholder="enter name">
+								</div>
+								<div class="form-group">
+									<input name="email" type="email" placeholder="enter email">
+								</div>
+								<div class="form-group">
+									<textarea name="message" placeholder="enter message"></textarea>
+								</div>
+								<div class="form-group">
+									<div class="button">
+										<button type="submit" class="btn primary">Submit</button>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ End Contact -->
+		
+		<!-- Footer -->
+		<footer id="footer" class="footer section">
+			<div class="footer-top">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="logo">
+								<img src="img/minilogo.jpg" alt="#">
+							</div>
+							<!-- Social -->
+							<ul class="social">
+								
+								<li><a href="https://www.instagram.com/htk_is/"><span class="fa fa-instagram"></span></a></li>
+								<li><a href="https://www.pinterest.ru/iskaalymov/_saved/"><span class="fa fa-pinterest-p"></span></a></li>
+							</ul>
+							<!-- End Social -->
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="copyright">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<p>© All rights (not) reserved :/</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!--/ End Footer -->
+		
+        <script src="js/jquery.min.js">  </script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.nav.js"></script>
+        <script src="js/jquery.slicknav.min.js"></script>
+        <script src="js/easing.min.js"></script>
+		<script src="js/jquery-appear.js"></script>
+        <script src="js/jquery.scrollUp.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+		<script src="js/jquery.counterup.min.js"></script>
+		<script src="js/isotope.pkgd.min.js"></script>
+		<script src="js/wow.min.js"></script>
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyC0RqLa90WDfoJedoE3Z_Gy7a7o8PCL2jw"></script>
+        <script type="text/javascript" src="js/gmaps.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
